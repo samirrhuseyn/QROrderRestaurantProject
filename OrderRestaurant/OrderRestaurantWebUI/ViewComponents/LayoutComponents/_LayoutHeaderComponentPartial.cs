9ab0dac4f13494a7420d7884f21189562d6242f1
Samir Hüseynli <samirrhuseyn@gmail.com>
2026-05-26 00:30:49 +0400

    Layout

---
+++ OrderRestaurant/OrderRestaurantWebUI/ViewComponents/LayoutComponents/_LayoutHeaderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderRestaurantWebUI.ViewComponents.LayoutComponents
+{
+    public class _LayoutHeaderComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(); 
+        }
+    }
+}
